d64c7256f1acfd2dd9a61528984c9c266ec49079
jacob <jacobomeara@icloud.com>
2024-09-30 14:12:57 -0400

    setup structure

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,23 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.ShooterCommand;
 
 public class RobotContainer {
+  // private final Joystick driver = new Joystick(0);
+  private final Joystick operator = new Joystick(1);
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    new JoystickButton(operator, 0).whileTrue(new ShooterCommand());
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
